b4e7494da06cd881b006db3186ed6c8b88584cdd
Lord-Valen <lord_valen@protonmail.com>
2025-07-10 22:10:12 -0400

    use newer kdenlive
    Who wants to compile opencv? Not me!

--- nixosProfiles/nle.nix
+++ nixosProfiles/nle.nix
@@ -1,12 +1,19 @@
-{ config, lib, pkgs, ... }: {
-  environment.systemPackages = (with pkgs; [
-    #libsForQt5.kdenlive
-    (libsForQt5.kdenlive.overrideAttrs
-      (_: previousAttrs: { version = "22.08.3"; }))
-    glaxnimate
-    mediainfo
-    mlt
-    #davinci-resolve
-    ffmpeg
-  ]);
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  environment.systemPackages = (
+    with pkgs;
+    [
+      kdePackages.kdenlive
+      glaxnimate
+      mediainfo
+      mlt
+      #davinci-resolve
+      ffmpeg
+    ]
+  );
 }
